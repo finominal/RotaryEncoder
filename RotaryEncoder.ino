322c5f19455e1d1778f9bb298fe1e2ac9bd60e24
Fin McCarthy <fin@ominal.com>
2014-04-04 23:35:38 +1100

    Initial Checkin
    Examaple of how to use a two pin Rotary Encoder.

---
+++ RotaryEncoder.ino
@@ -0,0 +1,77 @@
+// usually the rotary encoders three pins have the ground pin in the middle
+enum PinAssignments {
+  encoderPinA = 2,   // rigth
+  encoderPinB = 3,   // left
+  clearButton = 8    // another two pins
+};
+
+volatile unsigned int encoderPos = 0;  // a counter for the dial
+unsigned int lastReportedPos = 1;   // change management
+static boolean rotating=false;      // debounce management
+
+// interrupt service routine vars
+boolean A_set = false;              
+boolean B_set = false;
+
+
+void setup() {
+
+  pinMode(encoderPinA, INPUT); 
+  pinMode(encoderPinB, INPUT); 
+  pinMode(clearButton, INPUT);
+ // turn on pullup resistors
+  digitalWrite(encoderPinA, HIGH);
+  digitalWrite(encoderPinB, HIGH);
+  digitalWrite(clearButton, HIGH);
+
+// encoder pin on interrupt 0 (pin 2)
+  attachInterrupt(0, doEncoderA, CHANGE);
+// encoder pin on interrupt 1 (pin 3)
+  attachInterrupt(1, doEncoderB, CHANGE);
+
+  Serial.begin(9600);  // output
+}
+
+// main loop, work is done by interrupt service routines, this one only prints stuff
+void loop() { 
+  rotating = true;  // reset the debouncer
+
+  if (lastReportedPos != encoderPos) {
+    Serial.print("Index:");
+    Serial.println(encoderPos, DEC);
+    lastReportedPos = encoderPos;
+  }
+  if (digitalRead(clearButton) == LOW )  {
+    encoderPos = 0;
+  }
+}
+
+// Interrupt on A changing state
+void doEncoderA(){
+  // debounce
+  if ( rotating ) delay (1);  // wait a little until the bouncing is done
+
+  // Test transition, did things really change? 
+  if( digitalRead(encoderPinA) != A_set ) {  // debounce once more
+    A_set = !A_set;
+
+    // adjust counter + if A leads B
+    if ( A_set && !B_set ) 
+      encoderPos += 1;
+
+    rotating = false;  // no more debouncing until loop() hits again
+  }
+}
+
+// Interrupt on B changing state, same as A above
+void doEncoderB(){
+  if ( rotating ) delay (1);
+  if( digitalRead(encoderPinB) != B_set ) {
+    B_set = !B_set;
+    //  adjust counter - 1 if B leads A
+    if( B_set && !A_set ) 
+      encoderPos -= 1;
+
+    rotating = false;
+  }
+}
